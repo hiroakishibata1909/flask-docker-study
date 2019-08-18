1f904e8645ee3d685c91fc9313d4613234be45cb
DIR\hiroaki.shibata <hiroaki.shibata@accenture.com>
2019-08-18 21:37:32 +0900

    add healthcheck endpoint

--- Docker/app.py
+++ Docker/app.py
@@ -12,6 +12,10 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
+@app.route('/health', methods=['GET'])
+def healthcheck():
+    return 'Healthcheck is OK.', 200
+
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
